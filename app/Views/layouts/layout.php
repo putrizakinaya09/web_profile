9d8c5ca44eb58286d4dea9a3f3a4b8b02ab55597
Dedi Hartono <dedi.hartono@mad.co.id>
2022-04-27 18:39:53 +0700

    change navbar to dropdown

--- app/Views/layouts/layout.php
+++ app/Views/layouts/layout.php
@@ -10,7 +10,7 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-  <link rel="stylesheet" href="<?= base_url()?>/assets/css/carousel.css">
+  <link rel="stylesheet" href="<?= base_url() ?>/assets/css/carousel.css">
   <title>Web2 Kelompok 2</title>
   <style>
     .bd-placeholder-img {
@@ -34,7 +34,7 @@
   <header>
     <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
       <div class="container-fluid">
-        <a class="navbar-brand" href="#">Carousel</a>
+        <a class="navbar-brand" href="#">STMIK Mardira Indonesia</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
@@ -43,11 +43,21 @@
             <li class="nav-item">
               <a class="nav-link active" aria-current="page" href="#">Home</a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Profile</a>
+            <li class="nav-item dropdown">
+              <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Profile</a>
+              <div class="dropdown-menu">
+                <a href="#" class="dropdown-item">Sabutan KA.Prodi</a>
+                <a href="#" class="dropdown-item">Visi dan Misi</a>
+                <a href="#" class="dropdown-item">Struktur Organisasi</a>
+              </div>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Kegiatan</a>
+            <li class="nav-item dropdown">
+              <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Kegiatan</a>
+              <div class="dropdown-menu">
+                <a href="#" class="dropdown-item">Pengumuman</a>
+                <a href="#" class="dropdown-item">Berita</a>
+                <a href="#" class="dropdown-item">Gallery</a>
+              </div>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="#">Kurikulum</a>
@@ -55,6 +65,7 @@
             <li class="nav-item">
               <a class="nav-link" href="#">Kontak</a>
             </li>
+            
           </ul>
 
         </div>
